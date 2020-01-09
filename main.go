5e09780240c95fe680152254050d00468206c7af
Preston Kemp <preston@prestonkemp.com>
2020-01-09 12:35:01 -0600

    -rearrange roots -add a range protection calculation

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ import (
 )
 
 func getSha256FingerPrint(certificate *x509.Certificate) [sha256.Size]byte {
-	 //
-	 //fingerprint := hasher.Sum(certificate.Raw)
-	 //var fingerprint1 = sha1.Sum(certificate.Raw)
-	 //fmt.Println(fingerprint1)
-	 //return(fingerprint)
-	 return sha256.Sum256(certificate.Raw)
+	//
+	//fingerprint := hasher.Sum(certificate.Raw)
+	//var fingerprint1 = sha1.Sum(certificate.Raw)
+	//fmt.Println(fingerprint1)
+	//return(fingerprint)
+	return sha256.Sum256(certificate.Raw)
 }
 
 func convertBytesToCertificate(certificate []byte) *x509.Certificate {
@@ -52,6 +52,21 @@ fgUvpUszzRXqV9XSKx+bjXzOarbMAjEAt4HS4TuTzxFk3AsvF9Jt1dgF5FByYmXc
 pDzKYaUGmsn77cQwyXuJ4KW+Y1XmnBHj
 -----END CERTIFICATE-----`
 
+	const rootCA4 = `
+-----BEGIN CERTIFICATE-----
+MIIB6zCCAY+gAwIBAgIBATAMBggqhkjOPQQDAgUAMFsxCzAJBgNVBAYTAlVTMRgw
+FgYDVQQKEw9VLlMuIEdvdmVybm1lbnQxDDAKBgNVBAsTA0RvRDEMMAoGA1UECxMD
+UEtJMRYwFAYDVQQDEw1Eb0QgUm9vdCBDQSA0MB4XDTEyMDczMDE5NDgyM1oXDTMy
+MDcyNTE5NDgyM1owWzELMAkGA1UEBhMCVVMxGDAWBgNVBAoTD1UuUy4gR292ZXJu
+bWVudDEMMAoGA1UECxMDRG9EMQwwCgYDVQQLEwNQS0kxFjAUBgNVBAMTDURvRCBS
+b290IENBIDQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAR2yNhDyw8H0iwPKtA4
+8YLNQlXn3B1agLcIkUtU1k+yZoU0lo0uPvTgSpF8zM2GnxHgUqFmgsbLkCPsX1/1
+8DxFo0IwQDAdBgNVHQ4EFgQUvcG5a030HewwkL9ic8CEM/JxJIUwDgYDVR0PAQH/
+BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wDAYIKoZIzj0EAwIFAANIADBFAiEA6GGK
+99yqCaUH0kSeggNaRFNHhCOZz1zT3kpe1rs1NUYCIHYPuMR8FjV/1BLtiD2AEWtk
+B0xFZd9Trl8B7fFD0vW3
+-----END CERTIFICATE-----`
+
 	const rootCA3 = `
 -----BEGIN CERTIFICATE-----
 MIIDczCCAlugAwIBAgIBATANBgkqhkiG9w0BAQsFADBbMQswCQYDVQQGEwJVUzEY
@@ -73,21 +88,6 @@ Io+WBbRODHWRfdPy55TClBR2T48MqxCHWDKFB3WGEgte6lO0CshMhJIf6+hBhjy6
 9E5BStFsWEdBw4Za8u7p8pgnguouNtb4Bl6C8aBSk0QJutKpGVpYo6hdIG1PZPgw
 hxuQE0iBzcqQxw3B1Jg/jvIOV2gzEo6ZCbHw5PYQ9DbySb3qozjIVkEjg5rfoRs1
 fOs/QbP1b0s6Xq5vk3aY0vGZnUXEjnI=
------END CERTIFICATE-----`
-
-	const rootCA4 = `
------BEGIN CERTIFICATE-----
-MIIB6zCCAY+gAwIBAgIBATAMBggqhkjOPQQDAgUAMFsxCzAJBgNVBAYTAlVTMRgw
-FgYDVQQKEw9VLlMuIEdvdmVybm1lbnQxDDAKBgNVBAsTA0RvRDEMMAoGA1UECxMD
-UEtJMRYwFAYDVQQDEw1Eb0QgUm9vdCBDQSA0MB4XDTEyMDczMDE5NDgyM1oXDTMy
-MDcyNTE5NDgyM1owWzELMAkGA1UEBhMCVVMxGDAWBgNVBAoTD1UuUy4gR292ZXJu
-bWVudDEMMAoGA1UECxMDRG9EMQwwCgYDVQQLEwNQS0kxFjAUBgNVBAMTDURvRCBS
-b290IENBIDQwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAAR2yNhDyw8H0iwPKtA4
-8YLNQlXn3B1agLcIkUtU1k+yZoU0lo0uPvTgSpF8zM2GnxHgUqFmgsbLkCPsX1/1
-8DxFo0IwQDAdBgNVHQ4EFgQUvcG5a030HewwkL9ic8CEM/JxJIUwDgYDVR0PAQH/
-BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wDAYIKoZIzj0EAwIFAANIADBFAiEA6GGK
-99yqCaUH0kSeggNaRFNHhCOZz1zT3kpe1rs1NUYCIHYPuMR8FjV/1BLtiD2AEWtk
-B0xFZd9Trl8B7fFD0vW3
 -----END CERTIFICATE-----`
 
 	const rootCA2 = `
@@ -113,13 +113,11 @@ qKKV7YK5FtOeC4fCwfKI9WLfaN/HvGlR7bFc3FRUKQ8JOZqsA8HbDE2ubwp6Fknx
 v5HSOJTT9pUst2zJQraNypCNhdk=
 -----END CERTIFICATE-----`
 
-
 	roots := x509.NewCertPool()
 	err5 := roots.AppendCertsFromPEM([]byte(rootCA5))
 	err4 := roots.AppendCertsFromPEM([]byte(rootCA4))
 	err3 := roots.AppendCertsFromPEM([]byte(rootCA3))
 	err2 := roots.AppendCertsFromPEM([]byte(rootCA2))
-
 
 	if !err5 || !err4 || !err3 || !err2 {
 		panic("failed to parse root certificate")
@@ -136,7 +134,7 @@ v5HSOJTT9pUst2zJQraNypCNhdk=
 
 	opts := x509.VerifyOptions{
 		Roots: roots,
-		}
+	}
 
 	if _, err := cert.Verify(opts); err != nil {
 		panic("failed to verify certificate: " + err.Error())
@@ -155,7 +153,7 @@ func main() {
 		os.Exit(1)
 	}
 	pemfileinfo, _ := cert.Stat()
-	var size int64 = pemfileinfo.Size()
+	var size = pemfileinfo.Size()
 	pembytes := make([]byte, size)
 
 	buffer := bufio.NewReader(cert)
@@ -164,7 +162,7 @@ func main() {
 	concatcerts := strings.SplitAfter(certString, "-----END CERTIFICATE-----")
 	var certs [][]byte
 	for _, s := range concatcerts {
-		cert := strings.Split(s,"\n")
+		cert := strings.Split(s, "\n")
 		var tempString string
 		for _, c := range cert {
 			if strings.HasPrefix(c, "subject") || strings.HasPrefix(c, "issuer") || c == "" {
@@ -179,11 +177,12 @@ func main() {
 		tempString = ""
 	}
 
-	//fmt.Println(VerifyCertificate(certs[39]))
-	for _, k := range certs   {
-		fingerprint := getSha256FingerPrint(convertBytesToCertificate(k))
-		s:= fmt.Sprintf("%x", fingerprint)
-		fmt.Println(s)
+	for _, k := range certs {
+		if len(k) > 0 {
+			fingerprint := getSha256FingerPrint(convertBytesToCertificate(k))
+			s := fmt.Sprintf("%x", fingerprint)
+			fmt.Println(s)
+		}
 	}
 	//fingerprint := getSha256FingerPrint(convertBytesToCertificate(certs[0]))
 	//s:= fmt.Sprintf("%x", fingerprint)
